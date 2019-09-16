4f8345974d58bc126846e792910e1044a441d639
Nikolay Tarasenko <kolyatarasenko@ukr.net>
2019-09-16 15:25:47 +0300

    mobile menu & adaptive was added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // main elements
 const langItems = document.querySelectorAll('.languages__item');
 const langCurrent = document.querySelector('.languages__current');
+const nav = document.querySelector('.nav');
+const burger = document.querySelector('.burger');
 
 // function for display current (selected) language
 const displayCurrentLanguage = e => {
@@ -15,9 +17,23 @@ const displayCurrentLanguage = e => {
     langCurrent.textContent = e.target.textContent;
 };
 
+const toggleMenu = e => {
+    nav.classList.toggle('js-open');
+    document.body.classList.toggle('js-overflow');
+};
+
+const showMenu = () => {
+    if (document.documentElement.clientWidth >= 751 || window.innerWidth >= 768) {
+        document.body.classList.remove('js-overflow');
+    }
+};
+
 window.onload = function() {
     // listeners for language selection
     for (let i = 0; i < langItems.length; i ++) {
         langItems[i].addEventListener('click', displayCurrentLanguage);
     }
+
+    burger.addEventListener('click', toggleMenu);
+    window.addEventListener('resize', showMenu);
 };
